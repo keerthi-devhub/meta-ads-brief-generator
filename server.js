fb68fa9b91f70efacab40fd8081abb08cf77b6b8
Keerthi <ishakeerthyy@gmail.com>
2026-05-28 11:17:47 +0530

    Modify static file serving and root route handler
    Updated static file serving and root route response.

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 // Root Route
 app.get('/', (req, res) => {
